b537cb9d11f32ef6251c849b117d0e8c5fa089b6
Dipon-Biswas <114634509+dipon-biswas-099@users.noreply.github.com>
2022-11-30 12:26:28 +0600

    Add files via upload

---
+++ math.h header file/Abs functon.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a=-5;
+    int b= -10;
+    int ans, ans2;
+    ans = abs(a);
+    ans2 = abs(b);
+    printf("%d\n%d",ans,ans2);
+
+
+    return 0;
+}
---
+++ math.h header file/sqrt function.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a=25;
+    int ans = sqrt(a);
+    printf("%d",ans );
+
+    return 0;
+}
